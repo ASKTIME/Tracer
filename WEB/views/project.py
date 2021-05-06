0e5787ca2a705173597af06da3818b029930e284
ASKTIME <2987338453@qq.com>
2021-05-06 17:32:04 +0800

    展示项目样式完成

--- WEB/views/project.py
+++ WEB/views/project.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, HttpResponse, render
 from WEB.forms.project import ProjectModelForm
 from django.http import JsonResponse
+from WEB import models
 
 
 def project_list(request):
@@ -10,8 +11,34 @@ def project_list(request):
     # 设置默认get提交项目列表
     # post提交则是 创建项目
     if request.method == 'GET':
+        """
+        1.从数据库中获取两部分数据
+        我创建的	已星标 + 未星标
+        我创建的	已星标	+ 未星标
+        2.提取已星标
+        列表 = 循环 [我创建的] + 循环 [我创建的]  把已星标的数据提取出来
+        
+        得到三个列表  已星标的 我创建的 我参与的
+
+        """
+        project_dict = {'star': [], 'my': [], 'join': []}
+        my_project_list = models.Project.objects.filter(creator=request.tracer.user)
+        for row in my_project_list:
+            if row.star:
+                project_dict['star'].append(row)
+            else:
+                project_dict['my'].append(row)
+
+        join_project_list = models.ProjectUser.objects.filter(user=request.tracer.user)
+        # 这里需要注意对象的不同，这里传入project对象
+        for item in join_project_list:
+            if item.star:
+                project_dict['star'].append(item.project)
+            else:
+                project_dict['join'].append(item.project)
+
         form = ProjectModelForm(request)
-        return render(request, 'project_list.html', {'form': form})
+        return render(request, 'project_list.html', {'form': form, 'project_dict': project_dict})
 
     # 表单验证
     form = ProjectModelForm(request, data=request.POST, )
